5d30066ceea55cdd0a55162f2443a8061dcc92b5
daisymae <daisymae.orcadiafarm@gmail.com>
2020-05-06 16:20:00 -0500

    Initial commit

---
+++ src/com/cherylorcutt/springdemo/Coach.java
@@ -0,0 +1,5 @@
+package com.cherylorcutt.springdemo;
+
+public interface Coach {
+	public String getDailyWorkout();
+}
---
+++ src/com/cherylorcutt/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.cherylorcutt.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+}
